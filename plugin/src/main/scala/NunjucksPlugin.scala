4a6d4157a80100df8ce61019639215108adc5181
Nick Wilson <thenickwilson@gmail.com>
2019-05-03 10:16:49 +0100

    Update plugin

--- plugin/src/main/scala/NunjucksPlugin.scala
+++ plugin/src/main/scala/NunjucksPlugin.scala
@@ -7,11 +7,11 @@ import uk.gov.hmrc.nunjucks.plugin.BuildInfo
 object NunjucksPlugin extends AutoPlugin {
 
   val libSetting = if (SystemUtils.IS_OS_LINUX) {
-    "uk.gov.hmrc" % "play-nunjucks-linux" % BuildInfo.version
+    "uk.gov.hmrc" %% "play-nunjucks-linux" % BuildInfo.version
   } else if (SystemUtils.IS_OS_MAC_OSX) {
-    "uk.gov.hmrc" % "play-nunjucks-mac" % BuildInfo.version
+    "uk.gov.hmrc" %% "play-nunjucks-mac" % BuildInfo.version
   } else if (SystemUtils.IS_OS_WINDOWS) {
-    "uk.gov.hmrc" % "play-nunjucks-win32" % BuildInfo.version
+    "uk.gov.hmrc" %% "play-nunjucks-win32" % BuildInfo.version
   } else {
     throw new IllegalStateException(s"Incompatible OS: ${System.getProperty("os.name")}")
   }
