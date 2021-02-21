9e8f2f548f21faba818ef2592ba3fcc93f2a03fd
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2021-02-21 08:36:36 +0000

    PLATUI-536: Fix tests for Play 2.7, remove unused code

--- it-server/app/controllers/sub/TestController.scala
+++ it-server/app/controllers/sub/TestController.scala
@@ -2,9 +2,6 @@ package controllers.sub
 
 import com.google.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
-import play.api.routing.JavaScriptReverseRoute
-
-import scala.util.Try
 
 class TestController @Inject() (
                                  cc: ControllerComponents
